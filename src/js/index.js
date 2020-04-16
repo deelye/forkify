88364aa60ba830c99052131af4e2975ea15163ea
deelye <dee.yen.lye@gmail.com>
2020-04-16 12:10:48 +0100

    added searchview functions

--- src/js/index.js
+++ src/js/index.js
@@ -1,12 +1,31 @@
-import axios from 'axios';
+import Search from "./models/Search";
+import * as searchView from "./views/searchView";
+import { elements } from  "./views/base";
 
-async function getResults(query) {
-  try {
-    const res = await axios(`https://forkify-api.herokuapp.com/api/search?q=${query}`);
-    console.log(res.data.recipes);
-  } catch (error) {
-    alert(error);
-  };
+const state = {};
+
+const controlSearch = async () => {
+  // 1. Get query from view
+  const query = searchView.getInput();
+
+  if (query) {
+    // 2. New search object and add to state
+    state.search = new Search(query);
+
+    // 3. Prepare UI for results
+    searchView.clearInput();
+    searchView.clearResults();
+
+    // 4. Search for recipes
+    await state.search.getResults();
+
+    // 5. Render results on UI
+    searchView.renderResults(state.search.result);
+
+  }
 };
 
-getResults("pasta");
+elements.searchForm.addEventListener("submit", event => {
+  event.preventDefault();
+  controlSearch();
+});
