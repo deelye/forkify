3795bb5d88da071623c1bf488492df930a4cb962
deelye <dee.yen.lye@gmail.com>
2020-04-17 11:00:57 +0100

    likes model created

---
+++ src/js/models/Likes.js
@@ -0,0 +1,29 @@
+export default class Likes {
+  constructor() {
+    this.likes = [];
+  }
+
+  addLike(id, title, author, image) {
+    const like = {
+      id,
+      title,
+      author,
+      image
+    };
+    this.likes.push(like);
+    return like;
+  }
+
+  deleteLike(id) {
+    const index = this.likes.findIndex(element => element.id === id);
+    this.likes.splice(index, 1);
+  }
+
+  isLiked(id) {
+    return this.likes.findIndex(element => element.id === id) !== -1;
+  }
+
+  getNumberLikes() {
+    return this.likes.length;
+  }
+}
